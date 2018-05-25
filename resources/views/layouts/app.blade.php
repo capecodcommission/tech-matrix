bfb2048bc05be863c99422f4bd040a5da0b06165
Sue Malomo <suemalomo@My-MacBook-Pro.local>
2018-05-25 10:07:00 -0400

    add CSS class to nav-items

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,8 +36,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-						<li><a href="{{route('technologies.index')}}">Technologies</a></li>
-						<li><a href="{{route('inputs.index')}}">Inputs/Settings</a></li>
+						<li class="nav-item"><a href="{{route('technologies.index')}}">Technologies</a></li>
+						<li class="nav-item"><a href="{{route('inputs.index')}}">Inputs/Settings</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
