96894e4f6958cc5252f1eeee1f40cda361c22852
Sue Malomo <suemalomo@My-MacBook-Pro.local>
2018-05-25 10:05:51 -0400

    Add basic navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+						<li><a href="{{route('technologies.index')}}">Technologies</a></li>
+						<li><a href="{{route('inputs.index')}}">Inputs/Settings</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
