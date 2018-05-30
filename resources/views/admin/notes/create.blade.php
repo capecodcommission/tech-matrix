9e8835d8eff5534bf968420c44c85b0fa1207991
Sue Malomo <suemalomo@My-MacBook-Pro.local>
2018-05-30 11:08:00 -0400

    fix notes.create template links

--- resources/views/admin/notes/create.blade.php
+++ resources/views/admin/notes/create.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-md-8">
-			<p><a href="{{route('technologies.index')}}">Back to List</a></p>
+			<p><a href="{{route('notes.index')}}">Back to List</a></p>
 			<h2>Create New Note</h2>
 			<form action="{{route('notes.store')}}" method="POST">
 					@csrf
@@ -24,8 +24,8 @@
 @endsection
 
 @section('scripts')
-<link rel="stylesheet" href="/tech_matrix_v2/css/redactor.css" />
-<script src="/tech_matrix_v2/js/redactor.js"></script>
+<link rel="stylesheet" href="/css/redactor.css" />
+<script src="/js/redactor.js"></script>
 	<script type="text/javascript">
 		$(function()
 		{
