631cf61861254c785abd69daf3c40b56945f1d5c
Sue Malomo <suemalomo@my-macbook-pro.lan>
2018-06-01 11:47:09 -0400

    add FontAwesome icons

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,36 @@
 
 require("./bootstrap");
 
+// font awesome imports
+import fontawesome from "@fortawesome/fontawesome";
+import faPlay from "@fortawesome/fontawesome-free-solid/faPlay";
+import faVolumeUp from "@fortawesome/fontawesome-free-solid/faVolumeUp";
+import faPause from "@fortawesome/fontawesome-free-solid/faPause";
+import regular from "@fortawesome/fontawesome-pro-regular";
+import faLink from "@fortawesome/fontawesome-pro-light/faLink";
+import faShareAlt from "@fortawesome/fontawesome-free-solid/faShareAlt";
+import faFacebook from "@fortawesome/fontawesome-free-brands/faFacebookF";
+import faYouTube from "@fortawesome/fontawesome-free-brands/faYouTube";
+import faTwitter from "@fortawesome/fontawesome-free-brands/faTwitter";
+import faInstagram from "@fortawesome/fontawesome-free-brands/faInstagram";
+
+fontawesome.config = {
+	searchPseudoElements: true
+};
+
+fontawesome.library.add(
+	faPlay,
+	regular,
+	faLink,
+	faVolumeUp,
+	faPause,
+	faShareAlt,
+	faFacebook,
+	faYouTube,
+	faTwitter,
+	faInstagram
+);
+
 // window.Vue = require('vue');
 
 /**
