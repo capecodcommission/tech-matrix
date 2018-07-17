4fdd5a3e8dfdc51e953adf42112881e536b4b6e7
Sue Malomo <suemalomo@My-MacBook-Pro.local>
2018-07-17 09:45:30 -0400

    fix technologies sheet

--- app/Exports/TechnologiesSheet.php
+++ app/Exports/TechnologiesSheet.php
@@ -24,31 +24,9 @@ class TechnologiesSheet implements FromView, WithEvents
 
 	public function view(): View
 	{
-		$list = DB::select("
-		select t.*,
-		n.n_removed_low, n.n_removed_high, n.n_removed_avg,
-		np.n_kg_removed as n_removed_planning_period,
-		p.p_removed_low, p.p_removed_high, p.p_removed_avg,
-		pp.p_kg_removed as p_removed_planning_period,
-		pc.adj_project_cost_low,
-		pc.adj_project_cost_high,
-		pc.adj_project_cost_avg,
-		omc.adj_o_m_cost_low,
-		omc.adj_o_m_cost_high,
-		omc.adj_o_m_cost_avg,
-		pc.replacement_cost,
-		pc.total_replacement_cost,
-		pc.project_cost_pv
-		from technologies t 
-		left outer join N_Removed_Per_Year() n on t.id = n.id
-		left outer join P_removed_per_year() p on t.id = p.id
-		left outer join N_Reduction_Per_Planning_Period() np on t.id = np.id
-		left outer join P_Reduction_Per_planning_period() pp on t.id = pp.id
-		left outer join Project_Costs() pc on t.id = pc.id
-		left outer join Adjusted_O_M_Cost() omc on t.id = omc.id
-			 ");
+		
 		return view('export.technologies', [
-			'list' => $list
+			'list' => Technology::all()->sortBy('technology_id')
 		]);
 
 			// 'list' => Technology::all()->sortBy('technology_id')
