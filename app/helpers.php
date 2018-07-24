129cd9b739753a4c2c1532cea65559483ef33ea8
Sue Malomo <sue@bluegear.io>
2018-07-24 16:43:01 -0400

    convert advantages to array

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,7 @@
 	{
 		$value = str_replace('<ul>', '', $value);
 		$value = str_replace('</ul>', '', $value);
-		$value = str_replace('<li>', '', $value);
-		$value = str_replace('</li>', '\r\n', $value);
+		$value = str_replace('</li>', '', $value);
+		$value = explode('<li>', $value);
 		return $value;
 	}
