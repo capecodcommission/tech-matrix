016a2d67495f8d51a2ce9b299300b88c37556c83
Sue Malomo <sue@bluegear.io>
2018-07-31 12:45:02 -0400

    detail function in TechnologyController for api

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/detail/{id}', function () {
-    return new TechnologyResource(Technology::find($id));
-});
+Route::get('/detail/{id}', 'TechnologyController@detail');
 
 Route::get('/explore', function () {
     return new TechnologyCollection(Technology::all());
