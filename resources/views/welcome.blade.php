fdcafee5e6cde714d58d4d86e3421daaa13df124
Sue Malomo <sue@bluegear.io>
2018-07-31 12:22:12 -0400

    attempt Technology API creation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,16 +49,6 @@
                 font-size: 84px;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
@@ -81,14 +71,8 @@
                 <div class="title m-b-md">
                     Tech Matrix v2.0
                 </div>
-
-                {{-- <div class="links">
-                    <a href="/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> --}}
+				<div>For questions about the Tech Matrix, please contact Jay Detjens</div>
+               
             </div>
         </div>
     </body>
