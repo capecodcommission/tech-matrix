b432bbbbcfd4b339eff416a919da991e1637a62a
Sue Malomo <sue@bluegear.io>
2018-07-31 21:50:00 -0400

    remove withoutWrapper for ResourceCollection because it is ignored anyway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use \Maatwebsite\Excel\Sheet;
 use Illuminate\Http\Resources\Json\Resource;
-use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +19,6 @@ class AppServiceProvider extends ServiceProvider
             $sheet->getDelegate()->getStyle($cellRange)->applyFromArray($style);
 		});
 		 Resource::withoutWrapping();
-		 ResourceCollection::withoutWrapping();
     }
 
     /**
