d1c5e3557c5c4fba72fd58a6ac014c6c558a5401
Sue Malomo <sue@bluegear.io>
2018-08-03 11:05:59 -0400

    Attempt to limit Technology fields in Approach Resource Collection

--- app/Http/Resources/Approach.php
+++ app/Http/Resources/Approach.php
@@ -16,11 +16,23 @@ class Approach extends ResourceCollection
     {
 		
 		return [
-            'data' => $this->collection->transform(function($item){				
+            'data' => $this->collection->transform(function($item){		
+				$list = array();
+				foreach($item->technologies as $each) 
+				{
+					$list[$each->id]['id'] = $each->id;
+					$list[$each->id]['name'] = $each->technology_strategy;
+                    $list[$each->id]['name'] = $each->technology_strategy;
+					$list[$each->id]['icon'] = $each->icon;
+					$list[$each->id]['scales'] = $scales;
+					$list[$each->id]['n_removed_avg'] = round($each->calc->n_removed_avg);
+					$list[$each->id]['p_removed_avg'] = round($each->calc->p_removed_avg);
+					$list[$each->id]['useful_life_years'] = $each->useful_life_years;
+				}		
 				return [
                     'id' => $item->id,
 					'approach' => $item->approach,
-					'technologies' => $item->technologies
+					'technologies' => $list
                 ];
             }),
         ];
