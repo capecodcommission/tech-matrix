2642bcad98294e52c078464e57c398217b342e5c
Sue Malomo <sue@bluegear.io>
2018-09-04 10:47:57 -0400

    test concentration relationship

--- resources/views/admin/technologies/edit_relationships.blade.php
+++ resources/views/admin/technologies/edit_relationships.blade.php
@@ -101,7 +101,7 @@
 						@forelse($influent_concentrations as $each)	
 							<div class="form-check">
 								<label class="form-check-label" for="influent_concentrations_{{$each->id}}">
-									<input class="form-check-input form-control-lg" type="checkbox" value="{{$each->id}}" name="influent_concentrations[]" id="influent_concentrations_{{$each->id}}"  @if($item->influent_concentrations->contains($each->id)) checked='checked' @endif> {{$each->influent_concentration}}
+									<input class="form-check-input form-control-lg" type="checkbox" value="{{$each->id}}" name="influent_concentrations[]" id="influent_concentrations_{{$each->id}}"  @if($item->influent_sources->contains($each->id)) checked='checked' @endif> {{$each->influent_concentration}}
 								</label>
 							</div>
 						@empty
