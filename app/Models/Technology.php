0e717068d67cc9693544022233488d92223fcf4c
Sue Malomo <sue@bluegear.io>
2018-09-17 11:18:02 -0400

    add pivot baseline_concentration_id

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -32,7 +32,7 @@ class Technology extends Model
 
 	public function pollutants()
 	{
-		return $this->belongsToMany('App\Models\Pollutant', 'rel_baseline_concentrations', 'technology_id', 'pollutant_id')->withTimestamps();
+		return $this->belongsToMany('App\Models\Pollutant', 'rel_baseline_concentrations', 'technology_id', 'pollutant_id')->withPivot('baseline_concentration_id')->withTimestamps();
 	}
 	
 	public function influent_sources()
