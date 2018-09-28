2aee00f3fb2c454b147dd93d389a3db400ccbd05
Sue Malomo <sue@bluegear.io>
2018-09-28 12:03:56 -0400

    convert </p> to <br /> in export file

--- app/helpers.php
+++ app/helpers.php
@@ -2,19 +2,9 @@
 
 	function striphtml($value)
 	{
-		if(strpos($value, '<ul>'))
-		{
-			$value = str_replace('<ul>', '', $value);
-			$value = str_replace('</ul>', '', $value);
-			$value = str_replace('</li>', '', $value);
-			$value = explode('<li>', $value);
-		}
-		elseif(strpos($value, '<p>'))
-		{
-			$value = str_replace('</p>', '', $value);
-			$value = explode('<p>', $value);
-		}
-		
+			$value = str_replace('</p><p>', '<br /><br />', $value);
+			$value = str_replace('<p>', '', $value);
+			$value = str_replace('</p>', '', $value);		
 		// $value[0] = '';
 		return $value;
 	}
